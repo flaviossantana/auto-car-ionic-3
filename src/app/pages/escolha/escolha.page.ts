7bb7df6400bf886b209dffb58d663eaf44e4c8e3
Flávio Santana <9090flavio@gmail.com>
2019-09-26 00:10:14 -0300

    Criação da página de escolha de acessórios

--- src/app/pages/escolha/escolha.page.ts
+++ src/app/pages/escolha/escolha.page.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CarroModel} from '../../model/carro.model';
 
 
 @Component({
@@ -6,6 +8,25 @@ import {Component} from '@angular/core';
     templateUrl: 'escolha.page.html',
     styleUrls: ['escolha.page.scss']
 })
-export class EscolhaPage {
+export class EscolhaPage implements OnInit, OnDestroy {
+
+    carro: CarroModel;
+
+    constructor(private route: ActivatedRoute, private router: Router) {
+        this.route.queryParams.subscribe(params => {
+            if (this.router.getCurrentNavigation().extras.state) {
+                this.carro = this.router.getCurrentNavigation().extras.state.carro;
+            }
+        });
+    }
+
+
+    ngOnInit(): void {
+        console.log(this.carro);
+    }
+
+    ngOnDestroy(): void {
+        console.log('ngOnDestroy:EscolhaPage');
+    }
 
 }
