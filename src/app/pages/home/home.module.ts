ea17167c15c8f4ea0edf096efb85703f19d4cb13
Flávio Santana <9090flavio@gmail.com>
2019-09-25 22:48:26 -0300

    Criação da página de escolha de acessórios

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 
 import {HomePage} from './home.page';
 import {EscolhaModule} from '../escolha/escolha.module';
+import {HomeRoutingModule} from './home-routing.module';
 
 @NgModule({
     imports: [
@@ -13,14 +13,9 @@ import {EscolhaModule} from '../escolha/escolha.module';
         FormsModule,
         IonicModule,
         EscolhaModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: HomePage
-            }
-        ])
+        HomeRoutingModule
     ],
     declarations: [HomePage]
 })
-export class HomePageModule {
+export class HomeModule {
 }
