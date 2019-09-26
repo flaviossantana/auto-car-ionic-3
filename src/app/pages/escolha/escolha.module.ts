38e7d1b2e872bbdd1d4d21d2e5bdf917f50174b5
Flávio Santana <9090flavio@gmail.com>
2019-09-26 00:09:57 -0300

    Criação da página de escolha de acessórios

--- src/app/pages/escolha/escolha.module.ts
+++ src/app/pages/escolha/escolha.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {EscolhaPage} from './escolha.page';
 import {IonicModule} from '@ionic/angular';
 import {EscolhaRoutingModule} from './escolha-routing.module';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
     imports: [
+        CommonModule,
         IonicModule,
         EscolhaRoutingModule
     ],
